e9b0a9581bc44db147eb6ae044b25ae183928715
heitormvl <heitormvl12@gmail.com>
2025-10-14 22:05:42 -0300

    commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gaby.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
